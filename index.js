a534ebb60af7221bdfeb6d7bbdcc479fe6a0b651
Menachem Rosenblum <mendel@rosencrew.com>
2022-01-10 16:12:41 -0500

    COmpleted Assignement

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Mendel is the champion";
+
+document.body.append('h1');
